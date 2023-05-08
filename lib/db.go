997edd4513e2f343d024225cbe2a132694d10c70
shinesyang <1661217065@qq.com>
2023-05-08 04:43:00 -0400

    修改gorm支持的v1.20.x以上版本

--- lib/db.go
+++ lib/db.go
@@ -3,7 +3,7 @@ package lib
 import (
 	"fmt"
 	"github.com/asim/go-micro/v3/util/log"
-	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
 	"reflect"
 	"strings"
 )
@@ -16,12 +16,16 @@ func NewMyDb(db *gorm.DB) *MyDB {
 	return &MyDB{db}
 }
 
-func (d *MyDB) MyAutoMigrate(values ...interface{}) *gorm.DB {
-	db := d.Unscoped()
-	d.remove(values...)
+func (d *MyDB) MyAutoMigrate(values ...interface{}) error {
+	Migrator := d.Migrator()
+
+	// 自封装删除字段
+	if err := d.remove(Migrator, values...); err != nil {
+		return err
+	}
+
 	// 调用gorm.DB内置AutoMigrate方法
-	db = d.AutoMigrate(values...)
-	return db
+	return Migrator.AutoMigrate(values...)
 }
 
 func (d *MyDB) GetName(t string) string {
@@ -40,7 +44,7 @@ func (d *MyDB) GetName(t string) string {
 }
 
 // 自定义过滤.通过struct(gorm:"not_set;-")属性来删除不需要的表字段
-func (d *MyDB) remove(values ...interface{}) {
+func (d *MyDB) remove(migrator gorm.Migrator, values ...interface{}) error {
 	/*
 		自定义过滤.通过struct(gorm:"not_set;-")属性来删除不需要的表字段
 		not_set;-必须同时存在,
@@ -48,7 +52,6 @@ func (d *MyDB) remove(values ...interface{}) {
 		-在内置方法AutoMigrate中用于过滤不处理的字段,不然使用not_set删除的字段会重新创建
 	*/
 	for _, value := range values {
-		scope := d.NewScope(value)
 		reflectType := reflect.ValueOf(value).Type()
 		tableNameUpper := d.GetName(reflectType.String())
 		tableName := strings.ToLower(tableNameUpper)
@@ -57,6 +60,7 @@ func (d *MyDB) remove(values ...interface{}) {
 		for reflectType.Kind() == reflect.Slice || reflectType.Kind() == reflect.Ptr {
 			reflectType = reflectType.Elem()
 		}
+
 		/*
 			反射遍历获取字段及标签用于判断标签是否存在not_set.存在则删除该字段
 			这里通过json标签来获取filedName,所以在定义struct时,必须要定义json标签
@@ -64,41 +68,19 @@ func (d *MyDB) remove(values ...interface{}) {
 		for i := 0; i < reflectType.NumField(); i++ {
 			log.Info(fmt.Sprintf("当前标签属性: %v,当前字段名:%v", reflectType.Field(i).Tag, reflectType.Field(i).Name))
 			filedName := reflectType.Field(i).Tag.Get("json")
-			if scope.Dialect().HasTable(tableName) { // 判断表存在
-				if scope.Dialect().HasColumn(tableName, filedName) { // 判断字段存在
+			if migrator.HasTable(value) {
+				if migrator.HasColumn(value, filedName) {
 					tagName, ok := reflectType.Field(i).Tag.Lookup("gorm")
 					if ok {
 						if strings.Contains(tagName, "not_set") {
-							scope.Raw(fmt.Sprintf("ALTER TABLE %v DROP %v;", tableName, filedName)).Exec()
+							log.Infof("删除字段: %v", filedName)
+							return migrator.DropColumn(value, filedName)
 						}
 					}
 				}
+
 			}
 		}
-
-		// 注释老方法
-
-		//scope := db.NewScope(value)                // 实例Scope加载value对应的表
-		//tableName := scope.TableName()             // 获取表名
-		//quotedTableName := scope.QuotedTableName() // 引用到的表名
-		//// 当表存在时,判断字段是否存在not_set tag(标签),存在则删除该字段
-		//if scope.Dialect().HasTable(tableName) {
-		//	for _, field := range scope.GetModelStruct().StructFields {
-		//
-		//		if scope.Dialect().HasColumn(tableName, field.DBName) {
-		//			if field.IsNormal {
-		//				log.Infof("获取到字段的标签:%v", field.Tag.Get("gorm"), field.DBName)
-		//				tagName, ok := field.Tag.Lookup("gorm")
-		//				if ok {
-		//					if strings.Contains(tagName, "not_set") {
-		//						scope.Raw(fmt.Sprintf("ALTER TABLE %v DROP %v;", quotedTableName, field.DBName)).Exec()
-		//						//tagName = "-"
-		//						//d.SetTagToStruct(value, field.Name, tagName)
-		//					}
-		//				}
-		//			}
-		//		}
-		//	}
-		//}
 	}
+	return nil
 }
--- lib/go.mod
+++ lib/go.mod
@@ -4,10 +4,11 @@ go 1.18
 
 require (
 	github.com/asim/go-micro/v3 v3.7.1
-	github.com/jinzhu/gorm v1.9.16
+	gorm.io/gorm v1.23.2
 )
 
 require (
 	github.com/google/uuid v1.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
 )
